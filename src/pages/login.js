560307ce233c22435dd86a18252e26ca9048a3b4
corpuser <ntmthien2001@gmail.com>
2024-03-19 16:39:40 -0700

    fix login

--- src/pages/login.js
+++ src/pages/login.js
@@ -5,60 +5,27 @@ import axios from 'axios';
 import {useNavigate} from 'react-router-dom';
 
 function Login() {
-    const [waiting, setWaiting]=useState(false)
     const navigate=useNavigate();
     const pca = new PublicClientApplication({
       auth: {
         clientId: process.env.REACT_APP_AZURE_CLIENT_ID,
-        authority: 'https://login.microsoftonline.com/'+process.env.REACT_APP_AZURE_TENANT_ID,
-        //redirectUri: 'http://localhost:3002/fleet/login',
+        authority: 'https://login.microsoftonline.com/common'
       },
     });
     useEffect(() => {
-      if(localStorage.getItem('userInfo')&&localStorage.getItem('token'))navigate("deliveries")
+      if(localStorage.getItem('token'))navigate("account")
     }, []);
     const loginAzure = async () => {
-      navigate("account")
-    //   if(waiting)return
-    //   pca.loginPopup({prompt: "select_account"})
-    //   .then(async (res) => {
-    //     // Handle successful login
-    //     setWaiting(true)
-    //     axios({
-    //       url:process.env.REACT_APP_BACKEND+'fleet/login',
-    //       method:'POST',
-    //       timeout: 20000,
-    //       headers: {
-    //         'Content-Type': 'application/json'
-    //       },
-    //       data:JSON.stringify({
-    //         "azureToken":res.idToken
-    //       })
-    //     }).then((response)=>{
-    //       if(response.data.status==='success'){
-    //         localStorage.setItem('token', response.data.token)
-    //         localStorage.setItem('userInfo', JSON.stringify(response.data.userInfo))
-    //         //console.log(JSON.parse(localStorage.getItem('userInfo')))
-    //         //localStorage.removeItem('toRemove')
-    //         //localStorage.removeItem('token')
-    //         setWaiting(false)
-    //         navigate("deliveries")
-    //       }else if(response.data.status==='no user found'){
-    //         alert('User not found')
-    //         setWaiting(false)
-    //       }else{
-    //         alert("An error occured, please try again later")
-    //         setWaiting(false)
-    //       }
-    //     }).catch((error)=>{
-    //       alert("An error occured, please try again later")
-    //       setWaiting(false)
-    //     })
-    //   })
-    //   .catch((error) => {
-    //     // Handle login error
-    //     if(!String(error).includes("cancelled"))alert(`Login error: '${error}'`);
-    //   });
+      pca.loginPopup({prompt: "select_account"})
+      .then(async (res) => {
+        // Handle successful login
+        localStorage.setItem('token', 'A'+res.idToken) // 'A' for Azure
+        navigate("account")
+      })
+      .catch((error) => {
+        // Handle login error
+        if(!String(error).includes("cancelled"))alert(`Login error: '${error}'`);
+      });
     };
     const loginGoogle = async()=>{
 
@@ -71,7 +38,6 @@ function Login() {
             height:'100svh',
             justifyContent:'center',
             alignItems:'center', 
-            opacity:waiting?0.4:1,
             userSelect: 'none'
         }}>
           <img style={{width:'2in', paddingBottom:'1in'}} src={require('../assets/logo.png')} alt='logo'></img> 
